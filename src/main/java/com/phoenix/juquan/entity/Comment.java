f2d3f0716d9e06277b1a17678ec0aefcb6c72028
huasiming <2366881210@qq.com>
2022-01-21 17:13:39 +0800

    添加Comment.java

--- src/main/java/com/phoenix/juquan/entity/Comment.java
+++ src/main/java/com/phoenix/juquan/entity/Comment.java
@@ -1,4 +1,37 @@
 package com.phoenix.juquan.entity;
 
-public class Comment {
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ApiModel("Comment 评论")
+public class Comment implements Serializable {
+    @Id
+    @ApiModelProperty("id")
+    private Long id;
+
+    @ApiModelProperty("笔记id")
+    private String objectId;
+
+    @ApiModelProperty("用户剧圈号")
+    private String juQuanNum;
+
+    @ApiModelProperty("评论时间")
+    private String commentTime;
+
+    @ApiModelProperty("评论内容")
+    private String content;
+
+    @ApiModelProperty("评论点赞数")
+    private String likes;
+
 }
